1a323e2b8d869ad7dcc93baa5eba39216b0e7ae7
Denis-Kostoglodov <denis.kostoglodov.04@gmail.com>
2022-04-27 22:32:50 +0700

    Update

--- geometry.c
+++ geometry.c
@@ -1,86 +1,109 @@
-#include <stdio.h>
-#include <string.h>
 #include <ctype.h>
+//#include <libgeom/geom.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+//#include "funcs.c"
 #define MAX 201
 
-typedef struct Coord{
-	char coor[30];
+typedef struct Coord {
+    char coor[30];
 } co;
 
-int main(void){
-	char ch1[] = "Circle\0";
-	char ch2[] = "(, , )\0";
-	char str[MAX];
-	char name[10];
-	co coordin[10];
+// float dotnum(char *c){
+//	double a = atof(c);
+//	float b = (float)a;
+// return b;
+//}
 
-	int n = 0;
-	int k = 0;
+int main(void)
+{
+    char ch1[] = "circle\0";
+    char ch2[] = "( , )\0";
+    char str[MAX];
+    char name[10];
+    co coordin[10];
 
-	puts("Enter string:\n");
-	fgets(str, MAX - 1, stdin);
+    unsigned int n = 0;
+    int k = 0;
 
-	for(int i = 0; ch1[i] != '\0'; i++){
-		if(islower(str[0])){
-			puts("1st letter must be in upper case.\n");
-			break;
-		}
-		if(str[i] == ch1[i]){
-			n++;
-			name[i] = str[i];
-		}
-		k = i + 2;
-	}
+    puts("Enter string:\n");
+    fgets(str, MAX - 1, stdin);
 
-	int c = 0;
-	if(n != strlen(ch1)){
-		puts("Check name or bracket.");
-		c++;
-	}
+    for (int i = 0; ch1[i] != '\0'; i++) {
+        if (isupper(str[i])) {
+            str[i] = tolower(str[i]);
+            // puts("1st letter must be in upper case.\n");
+            // break;
+        }
+        if (str[i] == ch1[i]) {
+            n++;
+            name[i] = str[i];
+        }
+        k = i + 2;
+    }
+    name[0] = toupper(name[0]);
 
-	int m = 0;
-	int j = 0;
-	int comm = 0;
+    int c = 0;
+    if (n != strlen(ch1)) {
+        puts("Check name or bracket.");
+        c++;
+    }
 
-	for(int i = k; str[i] != ')'; i++){
+    int m = 0;
+    int j = 0;
+    int comm = 0;
+    // printf("%d\n", n);
+    // printf("%c\n", str[k]);
+    for (int i = k; str[i] != ')'; i++) {
+        if (c != 0)
+            break;
 
-		if(c != 0) break;
+        if (str[i] == ',') {
+            n++;
+            comm++;
+            // m++;
+            continue;
+        }
 
-		if(str[i] == ' '){
-			n++;
-			continue;
-		}
+        if (str[i] == '.' || isdigit(str[i])) {
+            coordin[m].coor[j] = str[i];
+            j++;
+        } else if (str[i] == ' ') {
+            m++;
+            n++;
+            // comm++;
+            j = 0;
+        } else {
+            puts("Check input data or commas.\n");
+            break;
+        }
+    }
+    // printf("%d", n);
+    int l = 0;
+    while (l < 3) {
+        if (comm < 1) {
+            puts("Too few args, try again.\n");
+            break;
+        }
 
-		if(str[i] == '.' || isdigit(str[i])){
-			coordin[m].coor[j] = str[i];
-			j++;
-		}
-		else if(str[i] == ','){
-			m++;
-			n++;
-			comm++;
-			j = 0;
-		}
-		else{
-			puts("Check input data or commas.\n");
-			break;
-		}
-	}
-	int l = 0;
-	while(l < 3){
-		if(comm < 2){
-			puts("Too few args, try again.\n");
-			break;
-		}
+        printf("Object: %s\n", name);
+        l++;
 
-		printf("Object: %s\n", name);
-		l++;
+        printf("Coordinates: (%s, %s)\n", coordin[0].coor, coordin[1].coor);
+        l++;
 
-		printf("Coordinates: (%s, %s)\n", coordin[0].coor, coordin[1].coor);
-		l++;
+        printf("Radius: %s\n", coordin[2].coor);
+        l++;
+    }
 
-		printf("Radius: %s\n", coordin[2].coor);
-		l++;
-	}
-	return 0;
+    //float cord1 = dotnum(coordin[0].coor);
+    //float cord2 = dotnum(coordin[1].coor);
+    //float cord3 = dotnum(coordin[2].coor);
+
+    //per_area(cord3);
+
+    //printf("\n%f\n%f\n%s\n", cord1, cord2, ch2);
+    return 0;
 }
