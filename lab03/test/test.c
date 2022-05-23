3de9dc6a1f46fb5912736a53dac12d7b4fb1ba8f
Denis-Kostoglodov <99592785+Denis-Kostoglodov@users.noreply.github.com>
2022-05-23 12:49:51 +0700

    Update test.c

--- lab03/test/test.c
+++ lab03/test/test.c
@@ -1,62 +1,92 @@
-#include <libgeom/geom.h>
 #include <ctest.h>
+#include <libgeo/check.h>
+#include <math.h>
+#include <stdio.h>
+#define size 128
 
-CTEST(ctest, test_close_bracket)
+CTEST(geometry_suite, simple_overlap)
 {
-    char str[] = "closebracket)";
-    int cbflagexpected = 12;
-    int cbflag = isClsBracket(str, 0);
-    ASSERT_EQUAL(cbflagexpected, cbflag);
+    float x_cir = 0, y_cir = 0, r = 3, x1_tr = 0, y1_tr = 0, x2_tr = 2, y2_tr = 5, x3_tr = 5, y3_tr = 0;
+    int exp;
+    exp = check(x_cir, y_cir, r, x1_tr, y1_tr, x2_tr, y2_tr, x3_tr, y3_tr);
+    int res;
+   if (((sqrt((x_sir - x1_tr) * (x_sir - x1_tr) + (y_sir - y1_tr) * (y_sir - y1_tr)) > r)
+        && (sqrt((x_sir - x2_tr) * (x_sir - x2_tr) + (y_sir - y2_tr) * (y_sir - y2_tr)) > r)
+        && (sqrt((x_sir - x3_tr) * (x_sir - x3_tr) + (y_sir - y3_tr) * (y_sir - y3_tr)) > r))
+        || ((sqrt((x_sir - x1_tr) * (x_sir - x1_tr) + (y_sir - y1_tr) * (y_sir - y1_tr)) < r)
+            && (sqrt((x_sir - x2_tr) * (x_sir - x2_tr) + (y_sir - y2_tr) * (y_sir - y2_tr)) < r)
+            && (sqrt((x_sir - x3_tr) * (x_sir - x3_tr) + (y_sir - y3_tr) * (y_sir - y3_tr)) < r))) {
+     	res = 0;
+    } else {
+        res = 1;
+    }
+    ASSERT_EQUAL(exp, res);
 }
 
-CTEST(ctest, test_open_bracket)
+CTEST(geometry_suite, simple_not_overlap)
 {
-    char str[] = "open(bracket";
-    int obflagexpected = 4;
-    int obflag = isOpnBracket(str);
-    ASSERT_EQUAL(obflagexpected, obflag);
+  float  x_cir = -3, y_cir = -3, r = 3, x1_tr = 4, y1_tr = 0, x2_tr = 5, y2_tr = 8, x3_tr = 6, y3_tr = 0;
+  int exp;
+  exp = check(x_cir, y_cir, r, x1_tr, y1_tr, x2_tr, y2_tr, x3_tr, y3_tr);
+  int res;
+  if (((sqrt((x_sir - x1_tr) * (x_sir - x1_tr) + (y_sir - y1_tr) * (y_sir - y1_tr)) > r)
+       && (sqrt((x_sir - x2_tr) * (x_sir - x2_tr) + (y_sir - y2_tr) * (y_sir - y2_tr)) > r)
+       && (sqrt((x_sir - x3_tr) * (x_sir - x3_tr) + (y_sir - y3_tr) * (y_sir - y3_tr)) > r))
+       || ((sqrt((x_sir - x1_tr) * (x_sir - x1_tr) + (y_sir - y1_tr) * (y_sir - y1_tr)) < r)
+           && (sqrt((x_sir - x2_tr) * (x_sir - x2_tr) + (y_sir - y2_tr) * (y_sir - y2_tr)) < r)
+           && (sqrt((x_sir - x3_tr) * (x_sir - x3_tr) + (y_sir - y3_tr) * (y_sir - y3_tr)) < r))) {
+    res = 0;
+  } else {
+    res = 1;
+  }
+    ASSERT_EQUAL(exp, res);
 }
 
-CTEST(ctest, test_comma)
+CTEST(geometry_suite, Circle_area)
 {
-    char str[] = "isinmystrcomma()";
-    int commaflag = isCorrectCntComma(str);
-    int expectedcntcomma = 0;
-    ASSERT_EQUAL(expectedcntcomma, commaflag);
+	float x_cir = -3, y_cir = -3, r = 3, x1_tr = 4, y1_tr = 0, x2_tr = 5, y2_tr = 8, x3_tr = 6, y3_tr = 0;
+    int res, exp;
+    float Pi;
+    res = circle_area(x_cir, y_cir, r, x1_tr, y1_tr, x2_tr, y2_tr, x3_tr, y3_tr);
+    Pi=3.14;
+	exp=r*r*Pi;
+    ASSERT_EQUAL(exp, res);
 }
 
-CTEST(ctest, area_test)
+CTEST(geometry_suite, Circle_perimeter.)
 {
-    float expectedarea = 28.27;
-    float areagetted = area(3);
-    ASSERT_EQUAL(expectedarea, areagetted);
+	float x_cir = -3, y_cir = -3, r = 3, x1_tr = 4, y1_tr = 0, x2_tr = 5, y2_tr = 8, x3_tr = 6, y3_tr = 0;
+    int res, exp;
+    res = circle_perimeter(x_cir, y_cir, r, x1_tr, y1_tr, x2_tr, y2_tr, x3_tr, y3_tr);
+    float Pi;
+    Pi=3.14;
+	exp=2*r*Pi;
+    ASSERT_EQUAL(exp, res);
 }
 
-CTEST(ctest, perimeter_test)
+CTEST(geometry_suite, Triangle_perimeter)
 {
-    float expectedperimeter= 18.85;
-    float perimetergetted = perimeter(3);
-    ASSERT_EQUAL(expectedperimeter, perimetergetted);
+	float x_cir = -3, y_cir = -3, r = 3, x1_tr = 4, y1_tr = 0, x2_tr = 5, y2_tr = 8, x3_tr = 6, y3_tr = 0;
+    int res, exp;
+    float a, b, c;
+	a = sqrt(((x2_tr - x1_tr) * (x2_tr - x1_tr) + (y2_tr - y1_tr) * (y2_tr - y1_tr)));
+  b = sqrt(((x3_tr - x2_tr) * (x3_tr - x2_tr) + (y3_tr - y2_tr) * (y3_tr - y2_tr)));
+  c = sqrt(((x1_tr - x3_tr) * (x1_tr - x3_tr) + (y1_tr - y3_tr) * (y1_tr - y3_tr)));
+	exp = (a+b+c);
+    res = triangle_perimeter(x_cir, y_cir, r, x1_tr, y1_tr, x2_tr, y2_tr, x3_tr, y3_tr);
+    ASSERT_EQUAL(exp, res);
 }
 
-CTEST(ctest, test_intersection)
+CTEST(geometry_suite, Triangle_area)
 {
-    struct circle c1 , c2;
-    c1.c1 = 1;
-    c1.c2 = 1;
-    c1.radius = 3;
-    c2.c1 = 2;
-    c2.c2 = 2;
-    c2.radius = 2;
-    int expectedintersection = 1;
-    int gettedinstersection = isintersection(c1,c2);
-    ASSERT_EQUAL(expectedintersection, gettedinstersection);
-}
-
-CTEST(ctest, test_tofloat)
-{
-    char* c = "0.123456";
-    float cgetted = charToFloat(c);
-    float cexpected = 0.123456;
-    ASSERT_EQUAL(cexpected, cgetted);
+	float x_cir = -3, y_cir = -3, r = 3, x1_tr = 4, y1_tr = 0, x2_tr = 5, y2_tr = 8, x3_tr = 6, y3_tr = 0;
+    int res, exp;
+    float a, b, c, p;
+	a = sqrt(((x2_tr - x1_tr) * (x2_tr - x1_tr) + (y2_tr - y1_tr) * (y2_tr - y1_tr)));
+  b = sqrt(((x3_tr - x2_tr) * (x3_tr - x2_tr) + (y3_tr - y2_tr) * (y3_tr - y2_tr)));
+  c = sqrt(((x1_tr - x3_tr) * (x1_tr - x3_tr) + (y1_tr - y3_tr) * (y1_tr - y3_tr)));
+  p = (a + b + c) / 2;
+	exp = sqrt(p * (p - a) * (p - b) * (p - c));
+    res = triangle_area(x_cir, y_cir, r, x1_tr, y1_tr, x2_tr, y2_tr, x3_tr, y3_tr);
+    ASSERT_EQUAL(exp, res);
 }
